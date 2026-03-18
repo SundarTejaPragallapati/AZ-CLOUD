abc637ae9f22fe1685dfc7fa3714e989728de048
Your Name <your-email@example.com>
2026-03-19 01:58:10 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT')
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY')
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER')
+
+    SQL_SERVER = os.environ.get('SQL_SERVER')
+    SQL_DATABASE = os.environ.get('SQL_DATABASE')
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME')
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD')
+
+    SQLALCHEMY_DATABASE_URI = (
+        'mssql+pyodbc://'
+        + SQL_USER_NAME + ':' + SQL_PASSWORD
+        + '@' + SQL_SERVER + ':1433/'
+        + SQL_DATABASE
+        + '?driver=ODBC+Driver+18+for+SQL+Server'
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+    CLIENT_ID = os.environ.get('CLIENT_ID')
+
+    AUTHORITY = "https://login.microsoftonline.com/common"
+    REDIRECT_PATH = "/getAToken"
+    SCOPE = ["User.Read"]
+
+    SESSION_TYPE = "filesystem"
